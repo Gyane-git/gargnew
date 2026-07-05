9444e1a8a568a6ad7e58920ce40377b7227482bb
Gyane-git <alpen850@gmail.com>
2026-07-05 12:10:38 +0545

    google signup and search api

--- app/api/google-auth/route.js
+++ app/api/google-auth/route.js
@@ -1,59 +1,15 @@
-// app/api/google-auth/route.ts
-import { NextRequest } from "next/server";
-import toast from "react-hot-toast";
-
 export async function POST(req) {
   try {
     const body = await req.json();
-    const { token } = body;
-    console.warn(token);
+    const backendRes = await fetch(new URL("/api/v1/auth/social/google-register", req.url), {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+    });
 
-    // Optional: verify token with Google
-    const googleRes = await fetch(
-      `https://oauth2.googleapis.com/tokeninfo?id_token=${token}`
-    );
-    const googleUser = await googleRes.json();
-    // console.log(googleUser);
-    // Forward to your real backend
-    const backendRes = await fetch(
-      "https://gargdental.omsok.com/api/v1/auth/social/google-register",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          token: token,
-          unique_id: googleUser.kid,
-          access_token: 0,
-        }),
-      }
-    );
-    // console.log(googleUser.email);
-    // console.log(googleUser.sub);
-    if (backendRes.ok) {
-      const data = await backendRes.json();
-      // console.warn("Data response !");
-      // console.log(data);
-      return new Response(JSON.stringify(data), { status: 200, success: true });
-    } else {
-      // console.log(JSON.stringify(backendRes.error));
-      // console.log(backendRes.status);
-      const data = await backendRes.json();
-      // console.warn("Error response !");
-      toast.error(data?.errors[0]?.message);
-      // console.log(data);
-      return new Response({
-        status: backendRes.status,
-        success: false,
-        message: "Something Went Wronng !",
-      });
-    }
+    const data = await backendRes.json();
+    return Response.json(data, { status: backendRes.status });
   } catch (error) {
-    // console.warn(`Google auth error: ${error}`);
-    return new Response(
-      JSON.stringify({ error: "Failed to authenticate", success: false }),
-      {
-        status: 500,
-      }
-    );
+    return Response.json({ success: false, errors: [{ message: "Failed to authenticate." }] }, { status: 500 });
   }
 }
