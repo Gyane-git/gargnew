4dc2f8d8dffd12f28a3d5f5b22eec1d456b7d1e2
Scilent <scilentknight512@gmail.com>
2026-07-12 13:53:56 +0545

    optimize layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,28 +1,3 @@
-// "use client";
-
-// import AdminHeaderBar from "@/components/admin-HeaderBar";
-// import SideHeaderBar from "@/components/admin-sidebar";
-
-// export default function AdminLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <div className="h-screen flex flex-col overflow-hidden">
-//       <AdminHeaderBar onToggleSidebar={undefined} />
-
-//       <div className="flex flex-1 overflow-hidden">
-//         <SideHeaderBar />
-
-//         <main className="flex-1 overflow-y-auto bg-gray-50">
-//           {children}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import AdminHeaderBar from "@/components/admin-HeaderBar";
